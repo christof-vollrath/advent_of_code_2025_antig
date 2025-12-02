9a6a692dd8b089b5ecaff3ead20a9d028176167b
christof <christof@taobits.net>
2025-12-02 22:34:10 +0100

    Day02 part2 solved

--- src/test/kotlin/Day02.kt
+++ src/test/kotlin/Day02.kt
@@ -95,3 +95,81 @@ class Day02Part1Test : BehaviorSpec({
     }
 })
 
+fun isInvalidId2(input: Long): Boolean {
+    return isInvalidId2(input.toString())
+}
+fun isInvalidId2(input: String): Boolean {
+    val len = input.length
+    for (l in 1..len / 2) {
+        if (len % l == 0) {
+            val pattern = input.substring(0, l)
+            val repeated = pattern.repeat(len / l)
+            if (input == repeated) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+fun sumInvalidIdsInRange2(range: String): Long {
+    val parts = range.split("-")
+    val start = parts[0].toLong()
+    val end = parts[1].toLong()
+    var sum = 0L
+    for (i in start..end) {
+        if (isInvalidId2(i)) {
+            sum += i
+        }
+    }
+    return sum
+}
+
+fun sumInvalidIdsInRanges2(input: String): Long {
+    return input.split(",")
+        .map { it.trim() }
+        .filter { it.isNotEmpty() }
+        .sumOf { sumInvalidIdsInRange2(it) }
+}
+
+class Day02Part2Test : BehaviorSpec({
+    Given("an int id") {
+        When("checking if it is invalid with the new rule") {
+            Then("it should correctly identify invalid and valid ids") {
+                forAll(
+                    row(12341234L, true),
+                    row(123123123L, true),
+                    row(1212121212L, true),
+                    row(1111111L, true),
+                    row(12345L, false),
+                    row(1212121L, false), // 7 chars, 12 repeated 3.5 times
+                    row(123123124L, false)
+                ) { input, expected ->
+                    isInvalidId2(input) shouldBe expected
+                }
+            }
+        }
+    }
+
+    Given("multiple ranges of ids") {
+        When("summing invalid ids with the new rule") {
+            Then("it should return the correct sum for the complex example") {
+                val input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224," +
+                        "1698522-1698528,446443-446449,38593856-38593862,565653-565659," +
+                        "824824821-824824827,2121212118-2121212124"
+                sumInvalidIdsInRanges2(input) shouldBe 4174379265L
+            }
+        }
+    }
+
+    Given("the puzzle input") {
+        When("calculating the sum of invalid ids with the new rule") {
+            Then("it should print the result") {
+                val input = readResource("day02Input.txt")!!
+                val result = sumInvalidIdsInRanges2(input)
+                println("Result for Day 2 Part 2: $result")
+                result shouldBe 40028128307L
+            }
+        }
+    }
+})
