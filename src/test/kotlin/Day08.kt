3592d4746f047fa1c3e3ed4c0980cfb2703716a9
christof <christof@taobits.net>
2025-12-26 23:02:17 +0100

    Day08Part1 alternative approach without UnionFind, turns out it is slightly slower

--- src/test/kotlin/Day08.kt
+++ src/test/kotlin/Day08.kt
@@ -1,5 +1,6 @@
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
+import kotlin.system.measureTimeMillis
 
 val exampleInputDay08 = """
 162,817,812
@@ -74,11 +75,11 @@ class UnionFind(n: Int) {
         parent.indices.filter { parent[it] == it }.map { size[it] }
 }
 
-fun buildCircuits(coords: List<Coord3>, sortedDistances: List<CoordsWithDistance>, connections: Int): List<Int> {
+fun buildCircuitsUnionFind(coords: List<Coord3>, sortedDistances: List<CoordsWithDistance>): List<Int> {
     val unionFind = UnionFind(coords.size)
     val coordToIndex = coords.withIndex().associate { it.value to it.index }
 
-    sortedDistances.take(connections).forEach { connection ->
+    sortedDistances.forEach { connection ->
         val i = coordToIndex[connection.coord1]!!
         val j = coordToIndex[connection.coord2]!!
         unionFind.union(i, j)
@@ -86,6 +87,43 @@ fun buildCircuits(coords: List<Coord3>, sortedDistances: List<CoordsWithDistance
 
     return unionFind.getSizes()
 }
+
+typealias Circuit = MutableSet<Coord3>
+
+fun buildCircuitsSets(coords: List<Coord3>, sortedDistances: List<CoordsWithDistance>): List<Int> {
+    val circuits = mutableSetOf<Circuit>()
+    val circuitMap = mutableMapOf<Coord3, Circuit>()
+    sortedDistances.forEach { connection ->
+        val circuit1 = circuitMap[connection.coord1]
+        val circuit2 = circuitMap[connection.coord2]
+        if (circuit1 != null && circuit2 != null && circuit1 !== circuit2) { // Merge
+            val (src, dest) = if (circuit1.size < circuit2.size) circuit1 to circuit2 else circuit2 to circuit1
+            dest.addAll(src)
+            src.forEach { coord -> circuitMap[coord] = dest }
+            circuits.remove(src)
+        } else if (circuit1 != null) {
+            circuit1.add(connection.coord2)
+            circuitMap.put(connection.coord2, circuit1)
+        } else if (circuit2 != null) {
+            circuit2.add(connection.coord1)
+            circuitMap.put(connection.coord1, circuit2)
+        } else {
+            val circuit = mutableSetOf(connection.coord1, connection.coord2)
+            circuits.add(circuit)
+            circuitMap.put(connection.coord1, circuit)
+            circuitMap.put(connection.coord2, circuit)
+        }
+    }
+    // Make sure that every box is in a circuit
+    coords.forEach { coord ->
+        if (coord !in circuitMap) {
+            circuits.add(mutableSetOf(coord))
+        }
+    }
+    return circuits.map { it.size }
+}
+
+fun buildCircuits(coords: List<Coord3>, sortedDistances: List<CoordsWithDistance>): List<Int> = buildCircuitsUnionFind(coords, sortedDistances)
 
 class Day08Part1Test : BehaviorSpec({
     Given("example input") {
@@ -99,9 +137,10 @@ class Day08Part1Test : BehaviorSpec({
                 Then("shortest distance should be at the begining") {
                     coordsWithDistance[0].coord1 shouldBe Coord3(162, 817, 812)
                     coordsWithDistance[0].coord2 shouldBe Coord3(425, 690, 689)
+                    coordsWithDistance.size shouldBe 20*19 / 2 // every coord with every other
                 }
             When("circuits for 10 connections") {
-                val sizes = buildCircuits(coords, coordsWithDistance, 10).sortedDescending()
+                val sizes = buildCircuits(coords, coordsWithDistance.take(10)).sortedDescending()
                 Then("should have found the right circuit sizes") {
                     sizes shouldBe listOf(5, 4, 2, 2, 1, 1, 1, 1, 1, 1, 1)
                 }
@@ -117,12 +156,43 @@ class Day08Part1Test : BehaviorSpec({
         val coords = parseListOfCoord3(input)
         When("sorting by distance and building circuits for 1000 connections") {
             val coordsWithDistance = sortedDistances(coords)
-            val sizes = buildCircuits(coords, coordsWithDistance, 1000).sortedDescending()
+            val sizes = buildCircuits(coords, coordsWithDistance.take(1000)).sortedDescending()
             Then("calculate the product of the three largest circuits") {
                 val result = sizes.take(3).reduce { acc, s -> acc * s }
                 println("Result: $result")
                 result shouldBe 32103
             }
+        }
+    }
+})
+
+class Day08PerformanceTest : BehaviorSpec({
+    Given("puzzle input") {
+        val input = readResource("day08Input.txt") ?: ""
+        val coords = parseListOfCoord3(input)
+        val coordsWithDistance = sortedDistances(coords).take(1000)
+
+        When("comparing performance") {
+            // Warm up
+            repeat(10) {
+                buildCircuitsSets(coords, coordsWithDistance)
+                buildCircuitsUnionFind(coords, coordsWithDistance)
+            }
+
+            val iterations = 100
+            val setsTime = measureTimeMillis {
+                repeat(iterations) {
+                    buildCircuitsSets(coords, coordsWithDistance)
+                }
+            }
+            val unionFindTime = measureTimeMillis {
+                repeat(iterations) {
+                    buildCircuitsUnionFind(coords, coordsWithDistance)
+                }
+            }
+
+            println("Average time (Sets): ${setsTime.toDouble() / iterations} ms") // 0.19 ms average
+            println("Average time (Union-Find): ${unionFindTime.toDouble() / iterations} ms") // 0.15 ms average
         }
     }
 })
