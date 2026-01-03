6b759177a9d338c7eceaf7f6caed69a3ba756211
christof <christof@taobits.net>
2026-01-03 21:02:21 +0100

    Day08Part2 solved (with two approaches)

--- src/test/kotlin/Day08.kt
+++ src/test/kotlin/Day08.kt
@@ -50,6 +50,8 @@ fun sortedDistances(coords: List<Coord3>): List<CoordsWithDistance> =
 class UnionFind(n: Int) {
     private val parent = IntArray(n) { it }
     private val size = IntArray(n) { 1 }
+    var numComponents = n
+        private set
 
     fun find(i: Int): Int {
         if (parent[i] == i) return i
@@ -68,6 +70,7 @@ class UnionFind(n: Int) {
                 parent[rootJ] = rootI
                 size[rootI] += size[rootJ]
             }
+            numComponents--
         }
     }
 
@@ -75,55 +78,57 @@ class UnionFind(n: Int) {
         parent.indices.filter { parent[it] == it }.map { size[it] }
 }
 
-fun buildCircuitsUnionFind(coords: List<Coord3>, sortedDistances: List<CoordsWithDistance>): List<Int> {
+fun buildCircuitsUnionFind(coords: List<Coord3>, sortedDistances: List<CoordsWithDistance>): Pair<List<Int>, Pair<Coord3, Coord3>?> {
     val unionFind = UnionFind(coords.size)
     val coordToIndex = coords.withIndex().associate { it.value to it.index }
+    var lastConnection: Pair<Coord3, Coord3>? = null
 
-    sortedDistances.forEach { connection ->
+    for (connection in sortedDistances) {
         val i = coordToIndex[connection.coord1]!!
         val j = coordToIndex[connection.coord2]!!
-        unionFind.union(i, j)
+        if (unionFind.find(i) != unionFind.find(j)) {
+            unionFind.union(i, j)
+            lastConnection = connection.coord1 to connection.coord2
+            if (unionFind.numComponents == 1) break
+        }
     }
 
-    return unionFind.getSizes()
+    return unionFind.getSizes() to lastConnection
 }
 
 typealias Circuit = MutableSet<Coord3>
 
-fun buildCircuitsSets(coords: List<Coord3>, sortedDistances: List<CoordsWithDistance>): List<Int> {
+fun buildCircuitsSets(coords: List<Coord3>, sortedDistances: List<CoordsWithDistance>): Pair<List<Int>, Pair<Coord3, Coord3>?> {
     val circuits = mutableSetOf<Circuit>()
     val circuitMap = mutableMapOf<Coord3, Circuit>()
-    sortedDistances.forEach { connection ->
-        val circuit1 = circuitMap[connection.coord1]
-        val circuit2 = circuitMap[connection.coord2]
-        if (circuit1 != null && circuit2 != null && circuit1 !== circuit2) { // Merge
+    
+    // Initialize: Every box is its own circuit
+    coords.forEach { coord ->
+        val circuit = mutableSetOf(coord)
+        circuitMap[coord] = circuit
+        circuits.add(circuit)
+    }
+
+    var recentConnection: Pair<Coord3, Coord3>? = null
+    for (connection in sortedDistances) {
+        val circuit1 = circuitMap[connection.coord1]!!
+        val circuit2 = circuitMap[connection.coord2]!!
+        
+        if (circuit1 !== circuit2) { // Merge
             val (src, dest) = if (circuit1.size < circuit2.size) circuit1 to circuit2 else circuit2 to circuit1
             dest.addAll(src)
             src.forEach { coord -> circuitMap[coord] = dest }
             circuits.remove(src)
-        } else if (circuit1 != null) {
-            circuit1.add(connection.coord2)
-            circuitMap.put(connection.coord2, circuit1)
-        } else if (circuit2 != null) {
-            circuit2.add(connection.coord1)
-            circuitMap.put(connection.coord1, circuit2)
-        } else {
-            val circuit = mutableSetOf(connection.coord1, connection.coord2)
-            circuits.add(circuit)
-            circuitMap.put(connection.coord1, circuit)
-            circuitMap.put(connection.coord2, circuit)
+            recentConnection = connection.coord1 to connection.coord2
+            
+            if (circuits.size == 1) break
         }
     }
-    // Make sure that every box is in a circuit
-    coords.forEach { coord ->
-        if (coord !in circuitMap) {
-            circuits.add(mutableSetOf(coord))
-        }
-    }
-    return circuits.map { it.size }
+    
+    return circuits.map { it.size } to recentConnection
 }
 
-fun buildCircuits(coords: List<Coord3>, sortedDistances: List<CoordsWithDistance>): List<Int> = buildCircuitsUnionFind(coords, sortedDistances)
+fun buildCircuits(coords: List<Coord3>, sortedDistances: List<CoordsWithDistance>): Pair<List<Int>, Pair<Coord3, Coord3>?> = buildCircuitsUnionFind(coords, sortedDistances)
 
 class Day08Part1Test : BehaviorSpec({
     Given("example input") {
@@ -140,7 +145,7 @@ class Day08Part1Test : BehaviorSpec({
                     coordsWithDistance.size shouldBe 20*19 / 2 // every coord with every other
                 }
             When("circuits for 10 connections") {
-                val sizes = buildCircuits(coords, coordsWithDistance.take(10)).sortedDescending()
+                val sizes = buildCircuits(coords, coordsWithDistance.take(10)).first.sortedDescending()
                 Then("should have found the right circuit sizes") {
                     sizes shouldBe listOf(5, 4, 2, 2, 1, 1, 1, 1, 1, 1, 1)
                 }
@@ -156,7 +161,7 @@ class Day08Part1Test : BehaviorSpec({
         val coords = parseListOfCoord3(input)
         When("sorting by distance and building circuits for 1000 connections") {
             val coordsWithDistance = sortedDistances(coords)
-            val sizes = buildCircuits(coords, coordsWithDistance.take(1000)).sortedDescending()
+            val sizes = buildCircuits(coords, coordsWithDistance.take(1000)).first.sortedDescending()
             Then("calculate the product of the three largest circuits") {
                 val result = sizes.take(3).reduce { acc, s -> acc * s }
                 println("Result: $result")
@@ -165,6 +170,38 @@ class Day08Part1Test : BehaviorSpec({
         }
     }
 })
+
+class Day08Part2Test : BehaviorSpec({
+    Given("example input") {
+        val coords = parseListOfCoord3(exampleInputDay08)
+        When("sorting by distance and building circuits for all connections") {
+            val coordsWithDistance = sortedDistances(coords)
+            When("circuits for all connections") {
+                val (_, recentConnection) = buildCircuits(coords, coordsWithDistance)
+                Then("first connection to connect all boxes should be correct") {
+                    recentConnection shouldBe (Coord3(216, 146, 977) to Coord3(117, 168, 530))
+                }
+                Then("x product should be right") {
+                    val result = recentConnection!!.first.x.toLong() * recentConnection.second.x
+                    result shouldBe 25272L
+                }
+            }
+        }
+    }
+    Given("puzzle input") {
+        val input = readResource("day08Input.txt") ?: ""
+        val coords = parseListOfCoord3(input)
+        When("circuits for all connections") {
+            val coordsWithDistance = sortedDistances(coords)
+            val (_, recentConnection) = buildCircuits(coords, coordsWithDistance)
+            Then("print and verify x product") {
+                val result = recentConnection!!.first.x.toLong() * recentConnection.second.x
+                println("Part 2 Result: $result")
+                result shouldBe 8133642976L
+            }
+        }
+    }})
+
 
 class Day08PerformanceTest : BehaviorSpec({
     Given("puzzle input") {
