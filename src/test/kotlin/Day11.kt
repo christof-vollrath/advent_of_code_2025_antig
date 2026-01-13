18e52e9b5b5b20aa002e3ed218573b7b4341d3ed
christof <christof@taobits.net>
2026-01-13 22:43:31 +0100

    Day11Part2 solved

--- src/test/kotlin/Day11.kt
+++ src/test/kotlin/Day11.kt
@@ -25,6 +25,33 @@ class Day11 {
 
         return dfs(start)
     }
+
+    fun countPathsWithConstraints(
+        graph: Map<String, List<String>>,
+        start: String,
+        end: String,
+        required: Set<String>
+    ): Long {
+        val memo = mutableMapOf<Pair<String, Set<String>>, Long>()
+
+        fun dfs(current: String, visitedRequired: Set<String>): Long {
+            val nextVisitedRequired = if (current in required) visitedRequired + current else visitedRequired
+            
+            if (current == end) {
+                return if (nextVisitedRequired.size == required.size) 1L else 0L
+            }
+            
+            val state = current to nextVisitedRequired
+            if (state in memo) return memo[state]!!
+
+            val outputs = graph[current] ?: return 0L
+            val paths = outputs.sumOf { dfs(it, nextVisitedRequired) }
+            memo[state] = paths
+            return paths
+        }
+
+        return dfs(start, emptySet())
+    }
 }
 
 class Day11Part1Test : BehaviorSpec({
@@ -64,6 +91,51 @@ class Day11Part1Test : BehaviorSpec({
 
             Then("it should find the correct number of paths") {
                 pathCount shouldBe 796
+            }
+        }
+    }
+})
+
+class Day11Part2Test : BehaviorSpec({
+    val exampleInput = """
+        svr: aaa bbb
+        aaa: fft
+        fft: ccc
+        bbb: tty
+        tty: ccc
+        ccc: ddd eee
+        ddd: hub
+        hub: fff
+        eee: dac
+        dac: fff
+        fff: ggg hhh
+        ggg: out
+        hhh: out
+    """.trimIndent()
+
+    Given("the example input") {
+        val day11 = Day11()
+        val graph = day11.parseInput(exampleInput)
+
+        When("counting paths from svr to out visiting dac and fft") {
+            val pathCount = day11.countPathsWithConstraints(graph, "svr", "out", setOf("dac", "fft"))
+
+            Then("it should find 2 paths") {
+                pathCount shouldBe 2
+            }
+        }
+    }
+
+    Given("the puzzle input") {
+        val day11 = Day11()
+        val input = readResource("day11Input.txt") ?: throw Exception("Input not found")
+        val graph = day11.parseInput(input)
+
+        When("counting paths from svr to out visiting dac and fft") {
+            val pathCount = day11.countPathsWithConstraints(graph, "svr", "out", setOf("dac", "fft"))
+
+            Then("it should find the correct number of paths") {
+                pathCount shouldBe 294053029111296L
             }
         }
     }
